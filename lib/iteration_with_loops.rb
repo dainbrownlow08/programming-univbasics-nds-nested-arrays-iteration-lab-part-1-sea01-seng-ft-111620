93c3070b6ae9073e84681abbbc84ddcb65e71743
Dain Brownlow <dain.brownlow@outlook.com>
2020-11-01 00:31:03 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,5 +1,13 @@
 def find_even_values(src)
   row_index = 0
-  while 
+  while row_index < src.count do
+    column_index = 0
+    while column_index < src[row_index].count do
+      if src[row_index][column_index].even?
+        p src[row_index][column_index]
+      end
+      column_index +=1
+    end
+  end  
 
 end
